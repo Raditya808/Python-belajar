d8ca70d93319c363d635ec1643dbef8fc21e96fe
Raditya808 <mraditradit808@gmail.com>
2025-12-07 17:49:21 +0700

    if dan else

---
+++ 22.IF ELSE STAEMENT PART 1/if statement.py
@@ -0,0 +1,33 @@
+# if else biasa
+# 1. ada if nya 
+# 2. ada kondisi 
+# 3. aksi 
+
+nama = input('masukan nama anda:')
+
+# Program sebelum nya 
+# if kondisi == "": aksi 
+# Program selanjut nya 
+
+
+# 1. If inline
+# ini jika kondisi nama nya adalah ucup 
+# maka output nya adalah kamu ganteng
+#if nama == "ucup": print("kamu ganteng")
+#print(f'Terima kasih {nama}') # ini kondisi jika selesai
+
+
+# 2. Program if dengan indentation
+#if nama =="ucup":
+    #print('kamu ganteng') # indentation
+    #print('kamu juga keren banget')# ini akan mengikuti kondisi diatas maka akan mengeluarkan output dengan indentation yang sama
+#print(f'Terima kasih {nama}') # program selesai
+
+
+# 3. Else statement
+if nama=="otong":
+        print('halo dari otong')
+    
+else:
+        print('kamu bukan otong kamu ga kerwen')
+print('Program selesai')
