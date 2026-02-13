00c08608a59cf193e7ce8c06c90322b61644a938
Raditya808 <mraditradit808@gmail.com>
2026-02-13 10:35:24 +0700

    () {}

---
+++ 38.Tuples and sets/main.py
@@ -0,0 +1,39 @@
+# tuples dan set
+#
+# tuples adalah seperti list menggunakan [] namun tuples menggunakan ()
+# tuples biasanya digunakan untuk mengirim kode program lain 
+# data tuples bisa diambil lewat indexing[] namun tidak bisa di manipulasi lewat insert(), extend(), index[0] = '', remove(), append() index(), sort(), reverese() dll 
+# tuples bisa di print lewat index namun tidak bisa di ganti 
+#
+# set adalah seperti list juga [] namun menggunakan {} 
+# set juga digunakan untuk mengirim data ke kode program lain 
+# beda dengan tuples () set bisa di manipulasi namun tidak bisa diambil / diganti lewat index index[0] = value
+# dan tidak bisa di print index
+
+
+
+
+
+
+# tuples
+datatuples = (1,2,3,4,5)
+print(datatuples) # output (1,2,3,4,5)
+
+# tuples and index 
+# output benar = 1 
+print(datatuples[0])
+
+
+# tuples and manipluations syntax
+# ini tidak bisa karena akan error
+#datatuples.append(1)
+
+
+
+# set 
+dataset = {1,2,3,4,5}
+print(dataset)
+
+# set and index print
+print(dataset[0])
+
