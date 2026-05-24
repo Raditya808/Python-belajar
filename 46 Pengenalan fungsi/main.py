3d9a16e8965810c5468813ee1830efc572961f5a
Raditya808 <mraditradit808@gmail.com>
2026-05-25 05:44:22 +0700

    function def

---
+++ 46 Pengenalan fungsi/main.py
@@ -0,0 +1,27 @@
+# fungsi 
+# fungsi merupakan sebuah kode dimana kita bisa membuat nya berulang hanya memanggil variabel aja 
+# dan fungsi seperti metode for loop tetapi lebih sederhana 
+# fungsi dalam python menggunakan syntax def
+
+
+# contoh 
+def tes():
+    # kita memiliki variabel tes dengan objek kosong dan objek ini yang akan di panggil berulang kali 
+    print('helo')
+    print('dunia')
+# ketika tes() ini di panggil atau di paste maka dia akan mengeluarkan output secara berulang tergantung jumlah dari pemanggilan objek nya 
+# ketika tes dibawah ini di panggil 2 kali maka dua output akan muncul
+tes()
+tes()
+
+
+
+# contoh ke 2 
+# pemanggilan objek harus setelah fungsi bukan sebelum seperti ini 
+
+
+# ini tidak boleh maka akan error
+#alo()
+
+#def alo():
+    #print('halo dunia')
